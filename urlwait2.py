e6db5124ef264e14ae8ca46a404971d15a0db414
Justin Wilson <justin.wilson@melbourneit.com.au>
2018-09-27 12:34:35 +1000

    fix print formatting error

--- urlwait2.py
+++ urlwait2.py
@@ -10,18 +10,18 @@ from requests import get
 def main(url):
     if not url.startswith('http'):
         url = 'http://' + url
-    print(f'urlwait2: waiting for {url}')
+    print('urlwait2: waiting for {0}'.format(url))
 
     attempt = 1
     while True:
         try:
             get(url, allow_redirects=True).raise_for_status()
         except Exception as e:
-            print(f'{e} Attempt {attempt} failed, retrying in 2 seconds')
+            print('{0} Attempt {1} failed, retrying in 2 seconds'.format(e, attempt))
             attempt += 1
             time.sleep(2)
         else:
-            print(f'urlwait2: successfully connected to {url}')
+            print('urlwait2: successfully connected to {0}'.format(url))
             break
 
 
